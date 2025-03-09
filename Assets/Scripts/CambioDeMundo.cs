4b2c699068b7ae0867557ad038dea70566300b8d
DGimenoX09 <david.gimeno.escobar@gmail.com>
2025-03-09 19:37:55 +0100

    nuevas cosas

--- Assets/Scripts/CambioDeMundo.cs
+++ Assets/Scripts/CambioDeMundo.cs
@@ -4,6 +4,9 @@ public class CambioDeMundo : MonoBehaviour
 {
     public GameObject escenario1; // Asigna el primer escenario en el Inspector
     public GameObject escenario2; // Asigna el segundo escenario en el Inspector
+    public GameObject filtro; // Asigna el objeto de filtro en el Inspector
+
+
 
     private GameObject escenarioActual;
 
@@ -13,6 +16,7 @@ public class CambioDeMundo : MonoBehaviour
         escenarioActual = escenario1;
         escenario1.SetActive(true);
         escenario2.SetActive(false);
+        filtro.SetActive(false); // Asegurate de que el filtro este desactivado al inicio
     }
 
     void Update()
@@ -32,6 +36,8 @@ public class CambioDeMundo : MonoBehaviour
             escenarioActual = escenario2;
             escenario1.SetActive(false);
             escenario2.SetActive(true);
+            filtro.SetActive(true); // Activa el filtro al cambiar al escenario 2
+            Debug.Log("Filtro activado");
             
 
             // Aumentar la posicion Y en 60 al cambiar al escenario 2
@@ -42,6 +48,7 @@ public class CambioDeMundo : MonoBehaviour
             escenarioActual = escenario1;
             escenario2.SetActive(false);
             escenario1.SetActive(true);
+            filtro.SetActive(false); // Desactiva el filtro al volver al escenario 1
             
 
             // Disminuir la posicion Y en 60 al volver al escenario 1
